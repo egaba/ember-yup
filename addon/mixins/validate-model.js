82eb1eea28ba0a30a16f11cc22670bec6956705c
Edward Gaba <egaba@underarmour.com>
2019-05-22 22:48:24 -0700

    save

--- addon/mixins/validate-model.js
+++ addon/mixins/validate-model.js
@@ -20,23 +20,35 @@ export default Mixin.create({
   validate() {
     return new RSVP.Promise((resolve, reject) => {
       const errors = this.get('errors');
+      const errorsByAttributeName = errors.errorsByAttributeName;
 
       this.set('isValidating', true);
 
-      const validate = this.get('_schema').validate(this.toJSON(), { abortEarly: false });
+      const validate = this.get('_schema').validate(this.toJSON(), {
+        abortEarly: false
+      });
 
       validate.then((data) => {
         this.set('isValidating', false);
-        errors.clear();
+
         resolve(data);
       }).catch((validations) => {
         this.set('isValidating', false);
 
-        validations.inner.forEach(function(validation) {
-          errors.set(validation.path, validation.errors);
+        errorsByAttributeName.forEach(function(k, name) {
+          errorsByAttributeName.get(name).clear();
         });
 
-        reject(errors);
+        validations.inner.forEach(function(validation) {
+          const attrErrors = errorsByAttributeName.get(validation.path);
+
+          attrErrors.clear();
+          attrErrors.addObjects(validation.errors);
+
+          errors.set(validation.path, attrErrors);
+        });
+
+        reject(errorsByAttributeName);
       });
     });
   },
---
+++ app/instance-initializers/setup-model-validation.js
@@ -0,0 +1,1 @@
+export { default, initialize } from 'ember-yup/instance-initializers/setup-model-validation';
