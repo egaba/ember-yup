7ea754837f45fbd83745f603a1883cb443912143
Edward Gaba <egaba@underarmour.com>
2019-05-31 21:23:02 -0700

    fix namespace

--- tests/dummy/app/initializers/fetch-api-data.js
+++ tests/dummy/app/initializers/fetch-api-data.js
@@ -1,9 +1,11 @@
 import fetch from 'fetch';
+import config from 'dummy/config/environment';
 
 export function initialize(application) {
   application.deferReadiness();
 
-  fetch('components-api.json').then(function(response) {
+  const namespace = config.environment === 'development' ? '/' : '/ember-yup/';
+  fetch(`${namespace}components-api.json`).then(function(response) {
     return response.json();
   }).then(function(data) {
     application.register('api:components', Ember.Object.extend(data));
