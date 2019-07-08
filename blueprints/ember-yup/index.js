cb7bd6f4ba6ea52cc74530e6e78ee0bcbac42c3a
Edward Gaba <egaba@underarmour.com>
2019-07-07 21:07:13 -0700

    remove afterInstall / beforeUninstall

--- blueprints/ember-yup/index.js
+++ blueprints/ember-yup/index.js
@@ -1,18 +1,4 @@
 /* eslint-env node */
 module.exports = {
   normalizeEntityName() {},
-
-  afterInstall() {
-    return this.addPackagesToProject([
-      { name: 'yup' },
-      { name: 'ember-cli-cjs-transform' },
-    ]);
-  },
-
-  beforeUninstall() {
-    return this.removePackagesFromProject([
-      { name: 'yup' },
-      { name: 'ember-cli-cjs-transform' },
-    ]);
-  }
 };
